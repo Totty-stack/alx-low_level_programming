96600408186b97c033e9c3fa9cca5e53fe93a275
totty-stack <amimokoths97@gmail.com>
2022-07-06 14:02:46 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@ void times_table(void)
 {
 	int rone, cone, d;
 	for (rone = 0; rone <= 9; rone++)
+	
 	{
 		_putchar('0');
 		_putchar(',');
@@ -25,11 +26,10 @@ void times_table(void)
 				_putchar(' ');
 			}
 			_putchar((d % 10) + '0');
-
 			if (cone < 9)
 			{
-				 _putchar(',');
-				 _putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
