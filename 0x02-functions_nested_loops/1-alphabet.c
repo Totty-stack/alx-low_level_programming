989ba8aa494619d3fedd67978e64a8ef868f40c9
totty-stack <amimokoths97@gmail.com>
2022-07-06 04:08:34 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,8 @@
  */
 void print_alphabet(void)
 {
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-		_putchar(alpha);
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 	_putchar('\n');
 }
