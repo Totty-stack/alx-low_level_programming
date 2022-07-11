87509ce9882766de1418b926ff76b0faaa9780f3
totty-stack <amimokoths97@gmail.com>
2022-07-11 13:42:54 -0700

    s

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "unistd.h"
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to print
+ *
+ * Return: on success i
+ * On error, -1 is returned and errno is returned appropriately
+ */
+int _putchar(char c)
+{
+	return (write( 1, &c, 1));
+}
+
