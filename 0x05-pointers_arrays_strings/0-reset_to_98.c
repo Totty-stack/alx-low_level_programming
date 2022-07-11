e7892ee48f25c98e41f6bc762d9a7bab553f855b
totty-stack <amimokoths97@gmail.com>
2022-07-11 12:58:26 -0700

    s

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset int - function that takes pointer and updates value to 98
+ * @n: value to reset
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
