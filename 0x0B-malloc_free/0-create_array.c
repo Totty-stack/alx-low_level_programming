03ac92a995a5903a9765b8ed9fef1c07b129ea60
totty-stack <amimokoths97@gmail.com>
2022-07-29 23:55:35 -0700

    s

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 
 /**
  * create_array - function that creates an array of chars
- * @c - character that initializes
- * @*size - size of array
+ * @c: character that initializes
+ * @size: size of array
  *
  * return: NULL if size == 0 or function fails
  *	return *p if fails
@@ -15,12 +15,12 @@ char *create_array(unsigned int size, char c)
 	char *array;
 
 	if (size == 0)
-		return(NULL);
+		return (NULL);
 
 	array = malloc(size * sizeof(char));
 
 		if (array == NULL)
-			return(NULL);
+			return (NULL);
 
 	for (index = 0; index < size; index++)
 		array[index] = c;
