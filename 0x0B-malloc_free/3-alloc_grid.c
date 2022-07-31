c4d355dea8be2d4a0e0b214ded47f10d25adf506
totty-stack <amimokoths97@gmail.com>
2022-07-31 04:50:11 -0700

    s

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,27 +11,42 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int c, d;
+	int c, d, e, f;
 	int **str;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	str = malloc(sizeof(int) * width);
-	
+
 	if (str == NULL)
+	{
+		free (str);
 		return (NULL);
-	
-	for  (d = 0; d < height; d++);
+	}
 
-	str[d] = malloc(sizeof(int) * height);
-	if  (str[d] == NULL)
-		return (NULL);
+	for  (d = 0; d < height; d++)
+	{
+		str[d] = malloc(sizeof(int) * height);
 
-	for (c = 0; c < width; c++);
-	for  (d = 0; d < height; d++);
-	str[c][d] = 0;
-	
-	return  (NULL);
+		if  (str[d] == NULL)
+		{
+			for (e = d; e >= 0; e--)
+			{
+				free (str[e]);
+			}
+			free (str);
+			return (NULL);
+		}
+	}
+	for (c = 0; c < width; c++)
+	{
+		for (f = 0; f < height; f++)
+		{
+			str[c][f] = 0;
+		}
+	}
+
+	return  (str);
 
 }
