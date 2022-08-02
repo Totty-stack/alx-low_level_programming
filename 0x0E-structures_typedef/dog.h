e7c2f6279230b039b3887c88a94ba2818f736bb7
totty-stack <amimokoths97@gmail.com>
2022-08-02 14:35:34 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,9 @@ struct dog *my_dog(char *name, char *owner, float age)
 	struct dog *my_dog;
 
 	my_dog = malloc(sizeof(struct dog));
+	if (my_dog == NULL)
+		return NULL;
+
 	my_dog->name = name;
 	my_dog->owner = owner;
 	my_dog->age = age;
