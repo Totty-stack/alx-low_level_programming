aa0e3a036d7f8664e5af468e384088a2f3a010bc
totty-stack <amimokoths97@gmail.com>
2022-08-02 14:37:42 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,7 @@
  * @owner: char data type
  * @age: int data type
  *
- * return: NULL on fail, my_dog on success
+ * return: my_dog.
  */
 
 struct dog
@@ -24,13 +24,9 @@ struct dog *my_dog(char *name, char *owner, float age)
 	struct dog *my_dog;
 
 	my_dog = malloc(sizeof(struct dog));
-	if (my_dog == NULL)
-		return NULL;
-
 	my_dog->name = name;
 	my_dog->owner = owner;
 	my_dog->age = age;
-
 	return (my_dog);
 }
 
